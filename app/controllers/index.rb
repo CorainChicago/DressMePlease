2143941e50ffc7af7901e18a2cdea4db99b6631d
LucyBoone <lucy.a.boone@gmail.com>
2015-11-05 17:28:46 -0600

    paths for new user and login added

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -5,3 +5,21 @@ end
 get '/index' do
   erb :index
 end
+
+get '/users/new' do
+  erb :'users/new'
+end
+
+post '/users' do
+  @user = User.create(params[:user])
+  if @user.valid?
+    session[:user_id] = @user.id
+    redirect "/users"
+  else @registration_error = true
+    erb :'users/new'
+  end
+end
+
+get '/login' do
+  erb :'users/show'
+end
