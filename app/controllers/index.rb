6b5fa7bb6c2ca4c5332633d387cb4e2d919c224e
KyleDolezal <Kyle.D.Dolezal@gmail.com>
2015-11-05 21:07:50 -0600

    Got weather summary to display and got icon to show up

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -5,13 +5,14 @@ get '/' do
 end
 
 get '/index' do
+  @weather = Forecast.get_forecast
   @time = Time.now.utc
   @quirk = Quirk.last
   text = @quirk.quote
-  @whattowear = Recommendation.new
-  @array = @whattowear.compile_recommendations
+  #@whattowear = Recommendation.new
+  #@array = @whattowear.compile_recommendations
 
-  @icon = Forecast.get_forecast.icon
+  # @icon = Forecast.get_forecast.icon
 
   # This is the code to pull from the Yoda API, after the '?sentence=' is the phrase the app will convert
   #The sentence needs to be in the format of 'word+word+word', a word with '+' before the next word
